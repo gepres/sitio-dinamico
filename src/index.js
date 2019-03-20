cdb11b279815022566840168a4161e847e9c97e5
gepres <genaropretill@gmail.com>
2019-03-20 14:20:19 -0500

    implementacion de codigo scss con media queries

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 import './js/config'
 
 import styles from './scss/main.scss'
-// import toggleNav from './js/toggle_nav'
-// import routes from './js/routes'
+import toggleNav from './js/toggle_nav'
+import routes from './js/routes'
 
 // const footerYear = document.querySelector('.Footer-year')
 
 // footerYear.textContent = new Date().getFullYear()
 
-// toggleNav()
-// routes()
+toggleNav()
+routes()
 
