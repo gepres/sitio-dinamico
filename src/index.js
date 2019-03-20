a7abdbcf60a18a42b39c440f519e02166759eeba
gepres <genaropretill@gmail.com>
2019-03-20 16:53:34 -0500

    arreglos en scss form photos pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import styles from './scss/main.scss'
 import toggleNav from './js/toggle_nav'
 import routes from './js/routes'
+import png from './img/logo.png'
 
 const footerYear = document.querySelector('.Footer-year')
 
